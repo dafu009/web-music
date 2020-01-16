1973a8bd89e9829fbe1a093357bfb012a070bf49
linshuyue <linshuyue@yy.com>
2020-01-16 12:05:59 +0800

    Auto stash before merge of "master" and "origin/master"

---
+++ src/components/loginRegister.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  .card-form
+    card
+</template>
+<script lang="ts">
+import Card from './Card.vue'
+import { Component, Vue, Prop } from 'vue-property-decorator'
+@Component({
+  components: {
+    Card
+  }
+})
+export default class loginRegister extends Vue {
+  
+}
+</script>
+<style lang="scss" scoped>
+  .card-form {
+    
+  }
+</style>
